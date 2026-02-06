6a0afd11d6c3d3a6d0267ab985e622bbb4285fcd
Paliga <80478964+Paliga@users.noreply.github.com>
2026-02-06 14:18:15 +0200

    add partials + app.get

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import express from express
+import express from 'express';
 
+const port = 3000;
 const app = express();
 
-app.listen(3000, () =>
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+app.listen(port, () =>
   console.log('Example app listening on port 3000!'),
 );
